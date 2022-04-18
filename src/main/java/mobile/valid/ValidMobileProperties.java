d740d12159cd21d409c61b72d0a766d2d8b30d6e
yslsml <mmf.antonovam@gmail.com>
2022-04-18 10:08:40 +0300

    feat: add isValidMobileBrand method

--- src/main/java/mobile/valid/ValidMobileProperties.java
+++ src/main/java/mobile/valid/ValidMobileProperties.java
@@ -1,5 +1,7 @@
 package mobile.valid;
 
+import mobile.entity.Brand;
+
 public class ValidMobileProperties {
     public static final double MIN_PRICE = 150;
 
@@ -9,8 +11,8 @@ public class ValidMobileProperties {
     public static final double MIN_PPI = 200;
     public static final double MAX_PPI = 600;
 
-    public static final double MIN_RAM = 64;
-    public static final double MAX_RAM = 1024;
+    public static final int MIN_RAM = 64;
+    public static final int MAX_RAM = 1024;
 
     public static boolean isValidDiagonalScreenSize(double value) {
         return (value >= MIN_DIAG_SCREEN_SIZE) && (value <= MAX_DIAG_SCREEN_SIZE);
@@ -20,11 +22,21 @@ public class ValidMobileProperties {
         return (value >= MIN_PPI) && (value <= MAX_PPI);
     }
 
-    public static boolean isValidRam(double value) {
-        return (value >= MIN_RAM) && (value <= MAX_RAM) && ((value%64) == 0) && (value%64 % 2 == 0) ;
+    public static boolean isValidRam(int value) {
+        return (value >= MIN_RAM) && (value <= MAX_RAM) && ((value%64) == 0) ;
     }
 
     public static boolean isValidPrice(double value) {
         return value >= MIN_PRICE ;
+    }
+
+    public static boolean isValidMobileBrand(String brandStr){
+        Brand[] brands = Brand.values();
+        for(Brand brand : brands){
+            if(brand.toString().equalsIgnoreCase(brandStr)){
+                return true;
+            }
+        }
+        return false;
     }
 }
